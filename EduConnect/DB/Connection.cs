bced864afe7a0d9cd1a5ad4cd3cb6475fa9dac69
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-14 16:22:58 -0300

    Criação da Conexão

---
+++ EduConnect/DB/Connection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace EduConnect.DB;
+
+internal class Connection : DbContext
+{
+    private string ConnectString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EduConnect;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectString);
+    }
+}
