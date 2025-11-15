0a581ee09f8ab2b3688959ce5cd881dcb565dcc1
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-15 11:28:33 -0300

    Movendo e criando pasta Models para os Objetos a serem utilizados

---
+++ EduConnect/Models/Professor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduConnect.Models;
+
+public class Professor
+{
+    [Key]
+    public required string Codigo { get; set; }
+    public required string Nome { get; set; }
+    public required string Email { get; set; }
+    public required string Telefone { get; set; }
+    public required string Status { get; set; }
+    public DateTime Nasc { get; set; }
+    public required List<string> Turmas { get; set; } 
+}
