6b1966f3992c99fb8d3ae7ed9c5771c7029ff900
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-15 11:05:47 -0300

    Criação do Objeto Professores

---
+++ EduConnect/Properties/Professor.cs
@@ -0,0 +1,12 @@
+﻿namespace EduConnect.Properties;
+
+public class Professor(string codigo, string nome, string email, string telefone, string status, DateTime nasc, Array turmas)
+{
+    public string Codigo { get; set; } = codigo;
+    public string Nome { get; set; } = nome;
+    public string Email { get; set; } = email;
+    public string Telefone { get; set; } = telefone;
+    public string Status { get; set; } = status;
+    public DateTime Nasc { get; set; } = nasc;
+    public Array Turmas { get; set; } = turmas;
+}
