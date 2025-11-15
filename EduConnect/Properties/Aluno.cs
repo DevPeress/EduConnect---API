7903e1b9ad25f1c8a6cc02240cfafbaf6a304158
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-15 11:05:37 -0300

    Criação do Objeto Aluno

---
+++ EduConnect/Properties/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace EduConnect.Properties;
+
+public class Aluno(string matricula, string nome, DateTime nascimento, string turma, string email, string telefone, string status, string media)
+{
+    public string Matricula { get; set; } = matricula;
+    public string Nome { get; set; } = nome;
+    public DateTime DataNascimento { get; set; } = nascimento;
+    public string Turma { get; set; } = turma;
+    public string Email { get; set; } = email;
+    public string Telefone { get; set; } = telefone;
+    public string Status { get; set; } = status;
+    public string MediaNotas { get; set; } = media;
+}
